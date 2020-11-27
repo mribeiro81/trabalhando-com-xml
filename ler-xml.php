f2a0357d4247cfe749a96e0392d79b315dc4ec4c
mribeiro81 <mribeiro81@hotmail.com>
2020-11-27 14:00:19 -0300

    Criei o arquivo ler-xml.php

--- ler-xml.php
+++ ler-xml.php
@@ -1,17 +1,89 @@
-<?php 
-/*
-Antes de executar esse comando, certifique-se de ter rodado antes o arquivo criar-xml.php, para que seja criado no arquivo clientes.xml que será lido aqui
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <title>Trabalhando com Ajax</title>
+    <link href="css/bootstrap/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/stylo.css" type="text/css" rel="stylesheet" /> 
+</head>
+<body>
 
-*/
+	<header class="container header-background mt-1">
 
-if(file_exists("clientes.xml")){
+	    <div class="row justify-content-md-center">
 
-	$arquivoXml = simplexml_load_file("clientes.xml"); //Aqui estou carregando o xml de um arquivo que criei, mas, poderia ser uma URL.
-	for($i = 0,$size = count($arquivoXml); $size > $i; $i++){
-		echo "Id_cliente: ".$arquivoXml->cliente[$i]->id_cliente.", Nome Cliente: ".$arquivoXml->cliente[$i]->nome_cliente.", Idade Cliente: ".$arquivoXml->cliente[$i]->idade_cliente."<br>";
-	}
+	        <div class="col-sm">
+	            <h2>Trabalhando com xml</h2>            
+	        </div>
 
-}
+	    </div>
 
+	</header>
 
-?>
+	<main role="main" class="container mt-2">
+
+		<div class="row">
+			<div class="col-sm">
+
+				<div class="alert alert-info mt-4">
+					<h3>Veja abaixo o resultado da leitura do arquivo xml.</h3>
+				</div>	
+
+				<div class="alert mt-3">
+					<?php 
+						/*
+						Lendo o arquivo xml
+						*/
+
+						if(file_exists("clientes.xml")){
+
+							$arquivoXml = simplexml_load_file("clientes.xml");
+							for($i = 0,$size = count($arquivoXml); $size > $i; $i++){
+								echo $arquivoXml->cliente[$i]->id_cliente.", ".$arquivoXml->cliente[$i]->nome_cliente.", ".$arquivoXml->cliente[$i]->idade_cliente."<br>";
+							}
+
+						}
+					?>
+				</div>
+			</div>	
+		</div>	
+
+		<div class="row">
+			<div class="col-sm">
+				<div class="alert alert-info mt-4">
+					<h3>Veja abaixo o conteúdo do arquivo xml.</h3>
+				</div>
+<pre>
+
+<&#63;xml version="1.0" encoding="utf-8"&#63;>
+&#60;clientes>
+	&#60;cliente>
+		&#60;id_cliente>1<&#47;id_cliente>
+		&#60;nome_cliente>Manoel<&#47;nome_cliente>
+		&#60;idade_cliente>39<&#47;idade_cliente>
+	&#60;/cliente>
+	&#60;cliente>
+		&#60;id_cliente>2<&#47;id_cliente>
+		&#60;nome_cliente>Daniella<&#47;nome_cliente>
+		&#60;idade_cliente>32<&#47;idade_cliente>
+	&#60;/cliente>
+	&#60;cliente>
+		&#60;id_cliente>3<&#47;id_cliente>
+		&#60;nome_cliente>Marisa<&#47;nome_cliente>
+		&#60;idade_cliente>56<&#47;idade_cliente>
+	&#60;/cliente>
+	&#60;cliente>
+		&#60;id_cliente>4<&#47;id_cliente>
+		&#60;nome_cliente>João<&#47;nome_cliente>
+		&#60;idade_cliente>64<&#47;idade_cliente>
+	&#60;/cliente>
+&#60;/clientes>
+
+</pre>				
+			</div>
+		</div>		
+	
+	</main>
+
+</body>
+</html>	
